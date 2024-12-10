45d9365b07d9de31cb89261632341f22b8a5b53a
Son Hyeok <thsgur1212@gmail.com>
2024-12-10 21:39:00 +0900

    :rocket: Feat: KakaoMapApi로 주변 가게 정보 조회후 저장

---
+++ src/main/java/what/what2eat/domain/store/controller/StoreController.java
@@ -0,0 +1,33 @@
+package what.what2eat.domain.store.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import what.what2eat.domain.store.controller.dto.StoreRequestDTO;
+import what.what2eat.domain.store.controller.dto.StoreResponseDTO;
+import what.what2eat.domain.store.service.StoreService;
+import what.what2eat.global.response.ApiResponse;
+
+import java.net.URISyntaxException;
+
+@Controller
+@Slf4j
+@RequestMapping("/api/v1/stores")
+@RequiredArgsConstructor
+public class StoreController {
+
+    private final StoreService storeService;
+
+    @GetMapping("")
+    public ResponseEntity<ApiResponse<StoreResponseDTO.StoreApiResultDTO>> getAllStores(@Valid @RequestBody StoreRequestDTO.StoreGetDTO request) throws URISyntaxException {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.ok(storeService.getNearbyPlaces(request)));
+    }
+
+}
