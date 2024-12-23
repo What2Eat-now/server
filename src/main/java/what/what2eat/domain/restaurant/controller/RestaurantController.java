ec662c009b7847f46fbf53fb0bde7f81a50d9607
Son Hyeok <thsgur1212@gmail.com>
2024-12-24 01:22:19 +0900

    :rocket: Feat: Swagger Setting

--- src/main/java/what/what2eat/domain/restaurant/controller/RestaurantController.java
+++ src/main/java/what/what2eat/domain/restaurant/controller/RestaurantController.java
@@ -1,5 +1,7 @@
 package what.what2eat.domain.restaurant.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,17 +22,20 @@ import java.net.URISyntaxException;
 @Slf4j
 @RequestMapping("/api/v1/restaurants")
 @RequiredArgsConstructor
+@Tag(name = "음식점 관련 컨트롤러", description = "주변 음식점 조회, 랜덤 음식점 추천 API")
 public class RestaurantController {
 
     private final RestaurantService restaurantService;
 
     @GetMapping("")
+    @Operation(summary = "주변 음식점 조회")
     public ResponseEntity<ApiResponse<RestaurantResponseDTO.RestaurantApiResultDTO>> getAllRestaurants(@Valid @RequestBody RestaurantRequestDTO.RestaurantGetDTO request) throws URISyntaxException {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ApiResponse.ok(restaurantService.getNearbyRestaurants(request)));
     }
 
     @GetMapping("/random")
+    @Operation(summary = "랜덤 음식점 조회", description = "주변 음식점 중에서 랜덤으로 하나를 지정해서 반환")
     public ResponseEntity<ApiResponse<RestaurantResponseDTO.RestaurantInfoDTO>> getRandomRestaurant(@RequestBody RestaurantRequestDTO.RestaurantGetDTO request) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ApiResponse.ok(restaurantService.getRandomRestaurant(request)));
