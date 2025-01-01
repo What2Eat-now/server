35ddaf24c370df3d751169fd3ead0bf8bc299c83
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2025-01-01 20:07:33 +0900

    🚀 Feat: 소셜 로그인 JWT 토큰 적용 (#19)
    * :rocket: Feature: 카카오 소셜 로그인 API 구현
    
    * :pushpin: Fix: 카카오 소셜 로그인 구조 변경
    
    * :pushpin: Fix: merge conflict 해결
    
    * :rocket: Feat: 소셜 로그인 JWT 토큰 적용
    
    * :pushpin: Fix: 유저 도메인 및 카카오 로그인 유효성 검사 로직 변경

--- src/main/java/what/what2eat/domain/auth/social/service/KakaoAuthService.java
+++ src/main/java/what/what2eat/domain/auth/social/service/KakaoAuthService.java
@@ -9,11 +9,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import what.what2eat.domain.auth.entity.Role;
 import what.what2eat.domain.auth.social.controller.dto.KakaoAuthResponseDTO;
 import what.what2eat.domain.auth.social.converter.KakaoAuthConverter;
 import what.what2eat.domain.auth.entity.Provider;
 import what.what2eat.domain.auth.entity.User;
 import what.what2eat.domain.auth.repository.AuthRepository;
+import what.what2eat.global.security.jwt.JwtProvider;
 
 @Service
 @RequiredArgsConstructor
@@ -29,6 +31,7 @@ public class KakaoAuthService {
     private final RestTemplate restTemplate;
     private final KakaoAuthConverter kakaoAuthConverter;
     private final AuthRepository authRepository;
+    private final JwtProvider jwtProvider;
 
     // 토큰 요청을 위한 Http 요청 객체 생성
     public HttpEntity<MultiValueMap<String, String>> createTokenRequest(String code) {
@@ -63,11 +66,11 @@ public class KakaoAuthService {
 
         // 토큰 조회
         KakaoAuthResponseDTO.KakaoTokenDTO tokenDTO = getAccessToken(code);
-        String accessToken = tokenDTO.getAccessToken();
+        String kakaoToken = tokenDTO.getAccessToken();
 
         // 인증을 위한 헤더 설정
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + accessToken);
+        headers.add("Authorization", "Bearer " + kakaoToken);
         headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
         HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(headers);
@@ -88,8 +91,16 @@ public class KakaoAuthService {
             authRepository.save(user);
         }
 
+        // accessToken 생성
+        String accessToken = jwtProvider.createAccessToken(userInfo.getKakaoAccount().getKakaoEmail(), Role.USER, Provider.KAKAO);
+
+        // refreshToken 생성
+        String refreshToken = jwtProvider.createRefreshToken(userInfo.getKakaoAccount().getKakaoEmail());
+
+
         return KakaoAuthResponseDTO.LoginInfoDTO.builder()
-                .token(tokenDTO)
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
                 .userInfo(userInfo)
                 .build();
     }
@@ -97,12 +108,14 @@ public class KakaoAuthService {
 
     // 카카오 로그인 정보 DB 저장 유무 확인
     public boolean validateKakaoAuth(KakaoAuthResponseDTO.KakaoUserInfoDTO userInfo) {
-        User findEmail = authRepository.findByUserEmail(userInfo.getKakaoAccount().getKakaoEmail()).get();
+        String kakaoUserEmail = userInfo.getKakaoAccount().getKakaoEmail();
 
-        if (findEmail != null && findEmail.getProvider() == Provider.KAKAO) {
+        // 계정이 존재할 경우
+        if (authRepository.existsByUserEmailAndProvider(kakaoUserEmail,Provider.KAKAO)) {
             return true;
         }
 
+        // 존재하지 않을 경우
         return false;
     }
 
