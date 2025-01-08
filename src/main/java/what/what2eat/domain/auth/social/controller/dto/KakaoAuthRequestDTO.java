8244b18de947b1a3473e0a1531929e5aa0cf5db6
Son Hyeok <thsgur1212@gmail.com>
2025-01-08 17:07:40 +0900

    :pushpin: Fix: 카카오 로그인 로직 변경

--- src/main/java/what/what2eat/domain/auth/social/controller/dto/KakaoAuthRequestDTO.java
+++ src/main/java/what/what2eat/domain/auth/social/controller/dto/KakaoAuthRequestDTO.java
@@ -1,4 +1,17 @@
 package what.what2eat.domain.auth.social.controller.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
 public class KakaoAuthRequestDTO {
+
+    @Builder
+    @Getter
+    public static class KakaoSignupDTO {
+        private String userEmail;
+
+        private String nickName;
+
+        private Long socialId;
+    }
 }
