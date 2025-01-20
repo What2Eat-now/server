a4bc9dbc2f8220d49dce8c2690ac2431fe028c5c
Son Hyeok <thsgur1212@gmail.com>
2025-01-20 16:29:07 +0900

    Fix: 다이어리 목록 조회 반환 속성 추가

--- src/main/java/what/what2eat/domain/diary/Converter/DiaryConverter.java
+++ src/main/java/what/what2eat/domain/diary/Converter/DiaryConverter.java
@@ -34,8 +34,7 @@ public class DiaryConverter {
         return DiaryResponseDTO.GetDiaryDTO.builder()
                 .title(diary.getTitle())
                 .content(diary.getContent())
-                .latitude(diary.getLocation().getY())
-                .longitude(diary.getLocation().getX())
+
                 .placeName(diary.getPlaceName())
                 .rate(diary.getRate())
                 .markerNumber(diary.getMarkerNumber())
@@ -49,6 +48,8 @@ public class DiaryConverter {
         return DiaryResponseDTO.GetDiaryThumbnailDTO.builder()
                 .diaryId(diary.getDiaryId())
                 .title(diary.getTitle())
+                .latitude(diary.getLocation().getY())
+                .longitude(diary.getLocation().getX())
                 .markerNumber(diary.getMarkerNumber())
                 .placeName(diary.getPlaceName())
                 .uploadImgList(convertImagesToUrls(diary.getUploadImgList()))
